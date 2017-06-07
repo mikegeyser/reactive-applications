ed596b3a1e29bd3d03ea379f3a7610f2274913d3
Mike Geyser <michaelg@bbd.co.za>
2017-06-07 10:45:19 +0200

    Some changes to the demo baseline

--- demo/app/src/app/app.component.ts
+++ demo/app/src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styles: []
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
+
+  ngOnInit() { }
 }
